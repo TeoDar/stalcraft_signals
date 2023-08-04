dd7f37d2c41ed7958df8ba6a480fc2894b7d454f
TeoDar <nikyl0777@gmail.com>
2023-08-04 08:22:46 +0300

    1

--- main.py
+++ main.py
@@ -7,96 +7,98 @@ import sys, os
 import winsound
 import pyautogui, keyboard
 from screeninfo import get_monitors
+from random import randint
 from time import sleep, strftime, localtime
 from multiprocessing import Process, set_start_method
 from ahk import AHK
 from ahk.directives import NoTrayIcon
 
 
-#set_start_method('spawn')
-ahk = AHK()#directives=[NoTrayIcon])
-print('Создал AHK')
-win = ahk.win_get("STALCRAFT")
-if not win:
-    exit("[Ошибка]: Не найдено окно STALCRAFT или игра не запущена")
-screen_width, screen_height = [(x.width, x.height) for x in get_monitors()][screen]  # 2560 1440 В моём случае
+class SignalFinder:
+    def __init__(self) -> None:
+        self.ahk = AHK(directives=[NoTrayIcon(apply_to_hotkeys_process=True)])
+        self.win = self.ahk.win_get("STALCRAFT")
+        if not self.win:
+            exit("[Ошибка]: Не найдено окно STALCRAFT или игра не запущена")
 
-last_proc: Process = None
-x_signal, y_signal = int(0.359375 * screen_width), int(0.368055 * screen_height)  # Координаты индикатора найденного сигнала
-x_med_rad, y_med_rad = int(0.5625 * screen_width), int(0.7361 * screen_height)  # Координаты кнопки "Средняя"
-x_tumbler, y_tumbler = int(0.703125 * screen_width), int(0.729166 * screen_height)  # Координаты тумблера начала сканирования
-x_search, y_search = int(0.328125 * screen_width), int(0.527777 * screen_height)  # Координаты кнопки "ПОИСК"
+        screen_width, screen_height = [(x.width, x.height) for x in get_monitors()][screen]
+        self.x_signal, self.y_signal = int(0.359375 * screen_width), int(0.368055 * screen_height)  # Координаты индикатора найденного сигнала
+        self.x_med_rad, self.y_med_rad = int(0.5625 * screen_width), int(0.7361 * screen_height)  # Координаты кнопки "Средняя"
+        self.x_tumbler, self.y_tumbler = int(0.703125 * screen_width), int(0.729166 * screen_height)  # Координаты тумблера начала сканирования
+        self.x_search, self.y_search = int(0.328125 * screen_width), int(0.527777 * screen_height)  # Координаты кнопки "ПОИСК"
 
-
-def reopen_suck(win):
-    win.send("x")
-    sleep(2)
-    win.send("x")
-
-
-def burn_signal():
-    print('Дошёл до перемещения')
-    ahk.mouse_move(x=x_med_rad, y=y_med_rad, speed=10, blocking=True)
-    ahk.click()
-    sleep(0.1)
-    ahk.mouse_move(x=x_signal, y=y_signal, speed=10, blocking=True)
-    ahk.click()
-    sleep(0.1)
-    ahk.mouse_move(x=x_signal, y=y_signal, speed=10, blocking=True)
-    ahk.click()
-    sleep(0.1)
-    print('Переместил')
-
-
-def searching():
-    win.activate()
-    win.send("x")
-    sleep(0.5)
-    r, g, b = pyautogui.pixel(x_signal, y_signal)  # Цвет индикатора НЕнайденного сигнала
-    print(f"Начальный цвет индикатора: [R:{r} G:{g} B:{b}]")
-    s_color_summ = r + g + b  # Вычисление суммы светов для дальнейшего сравнения и обнаружения найденного сигнала
-    s = ['.','..','...','....']
-    i = 0
-    while True:
-        sleep(1.5)
-        r, g, b = pyautogui.pixel(x_signal, y_signal)
-        if r + g + b > s_color_summ:
-            print(strftime("\n[%H:%M:%S]", localtime()), end=": ")
-            print(f"Сигнал найден!\nЦвет индикатора: [R:{r} G:{g} B:{b}]")
-            winsound.MessageBeep()
-            burn_signal()
-            break
+        self.searching_active = False
+        self.ahk.add_hotkey(keyname="f3", callback=self.manager)
+        self.ahk.start_hotkeys()
+        keyboard.wait("f4")
+        subprocess.run(["taskkill", "/F", "/IM", "AutoHotkey.exe"])
+    
+    def manager(self):
+        if self.searching_active:
+            self.searching_active = False
+            print("\n[Поиск остановлен]")
         else:
-            sys.stdout.write("\r" + "Поиск" + s[i])
-            sys.stdout.flush()
-            reopen_suck(win)
-            if i == 3:i = 0
-            else: i+=1
-    print('Поиск окончен')
+            print("[Запуск поиска]")
+            self.searching_active = True
+        self.searching()
 
+    def searching(self):
+        if self.searching_active == False: return
+        self.win.activate()
+        sleep(0.5)
+        self.win.send("x")
+        sleep(1)
+        r, g, b = pyautogui.pixel(self.x_signal, self.y_signal)  # Цвет индикатора НЕнайденного сигнала
+        print(f"Начальный цвет индикатора: [R:{r} G:{g} B:{b}]")
+        s_color_summ = r + g + b  # Вычисление суммы светов для дальнейшего сравнения и обнаружения найденного сигнала
+        s = [".", "..", "...", "...."]
+        i = 0
+        while self.searching_active:
+            sleep(1.5)
+            r, g, b = pyautogui.pixel(self.x_signal, self.y_signal)
+            if r + g + b > s_color_summ:
+                print(strftime("\n[%H:%M:%S]", localtime()), end=": ")
+                print(f"Сигнал найден!\nЦвет индикатора: [R:{r} G:{g} B:{b}]")
+                winsound.MessageBeep()
+                self.burn_signal()
+                print("Поиск окончен")
+                self.searching_active = False
+                break
+            else:
+                print("Поиск" + s[i], end="\r")
+                sys.stdout.flush()
+                self.reopen_suck()
+                if i == 3:
+                    i = 0
+                    print("Поиск     ", end="\r")
+                else:
+                    i += 1
 
-def start_stop():
-    """Функция управляющия потоками для регулирования запуска/остановки"""
-    global last_proc
-    if last_proc is None: last_proc = Process(target=searching)
-    if last_proc.is_alive():
-        last_proc.terminate()
-        last_proc.join()
-        last_proc = None
-        print("\n[Поиск остановлен]")
-    else:
-        print("[Запуск поиска]")
-        last_proc.start()
+    def burn_signal(self):
+        print("Дошёл до перемещения")
+        # Переключение на среднюю дистанцию
+        pyautogui.moveTo(x=self.x_med_rad, y=self.y_med_rad, duration=0.5)
+        self.win.click(x=self.x_med_rad, y=self.y_med_rad, blocking=True)
+        # Запуск сканирования
+        pyautogui.moveTo(x=self.x_tumbler, y=self.y_tumbler, duration=0.5)
+        self.win.click(x=self.x_tumbler, y=self.y_tumbler, blocking=True)
+        # Ожидание до 6 дальности
+        sleep(14)#randint(2, 4) * 0.1 + 20)
+        # Отключение сканирования
+        self.win.click(x=self.x_tumbler, y=self.y_tumbler, blocking=True)
+        # Нажатие на кнопку ПОИСК
+        pyautogui.moveTo(x=self.x_search, y=self.y_search, duration=0.5)
+        self.win.click(x=self.x_search, y=self.y_search, blocking=True)
+        print("Переместил")
+
+    def reopen_suck(self):
+        self.win.send("x")
+        sleep(2)
+        self.win.send("x")
 
 
 def main():
-    print('Запуск мэйна')
-    global last_proc
-    keyboard.add_hotkey("f3", start_stop)
-    keyboard.wait("f4")
-    if last_proc:
-        last_proc.terminate()
-    subprocess.run(['taskkill', '/F', '/IM', 'AutoHotkey.exe'])
+    SignalFinder()
 
 
 if __name__ == "__main__":
@@ -108,6 +110,5 @@ if __name__ == "__main__":
            |Для [Запуска]/[Остановки] нажмите "F3"|
             |Для [Завершения работы] нажмите "F4"|
              ‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾
-"""
-    )
+    """)
     main()
--- see_color.py
+++ see_color.py
@@ -5,12 +5,12 @@
 import pyautogui
 
 print("Нажми Ctrl-C чтобы выйти.")
-x, y = (688, 398)  # Координаты заданы вручную
-pyautogui.moveTo(x, y)
+#x, y = (688, 398)  # Координаты заданы вручную
+#pyautogui.moveTo(x, y)
 
 try:
     while True:
-        # x, y = pyautogui.position() # Для определения цвета под курсовом
+        x, y = pyautogui.position() # Для определения цвета под курсовом
         r, g, b = pyautogui.pixel(x, y)
         ColorStr = "R: " + str(r).rjust(4) + " G: " + str(g).rjust(4) + " B: " + str(b).rjust(4)
         print(ColorStr, end="")
