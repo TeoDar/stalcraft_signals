92713d0c5ef41f923d6a29480b64735de8ddce86
@FNNikulin <fnnikulin@skr.local>
2024-07-25 17:04:22 +0300

    #

--- app/main.py
+++ app/main.py
@@ -1,7 +1,26 @@
 # Для преобразования ui файла: python -m PyQt6.uic.pyuic -o ./app/interface.py -x ./app/interface.ui
 
+from PyQt6.QtGui import QDialog
+from ui_imagedialog import Ui_ImageDialog
+
+
 def main():
     pass
 
 if __name__ == "__main__":
     main()
+
+
+class ImageDialog(QDialog, Ui_ImageDialog):
+    def __init__(self):
+        super().__init__()
+
+        # Set up the user interface from Designer.
+        self.setupUi(self)
+
+        # Make some local modifications.
+        self.colorDepthCombo.addItem("2 colors (1 bit per pixel)")
+
+        # Connect up the buttons.
+        self.okButton.clicked.connect(self.accept)
+        self.cancelButton.clicked.connect(self.reject)
