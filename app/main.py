c3d59d62875cb470f3da987676d442051eeedde6
TeoDar <nikyl0777@gmail.com>
2024-08-04 22:02:16 +0300

    Добавлена настрйока задержки между кликами по САК

--- app/main.py
+++ app/main.py
@@ -1,4 +1,6 @@
 # Для преобразования ui файла: python -m PyQt6.uic.pyuic -o ./app/window.py -x interface.ui
+# Билд приложения через auto-py-to-exe. Настройки билда в build.json
+
 
 from PyQt6.QtWidgets import QApplication
 from config import Configuration, exception
